865cca94b89f2974c8b46760e2c56d3ae0153f3d
rayner <raynerweb@gmail.com>
2021-07-03 22:22:09 -0300

    add TarefaViewController

--- EstudandoViper/View/EstoriaViewController.swift
+++ EstudandoViper/View/EstoriaViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class EstoriaViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Estorias"
     }
 
+    @IBAction func nextButton(_ sender: Any) {
+        let tarefaViewController = TarefaViewController()
+        navigationController?.pushViewController(tarefaViewController, animated: true)
+    }
 
 }
---
+++ EstudandoViper/View/TarefaViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TarefaViewController.swift
+//  EstudandoViper
+//
+//  Created by rayner on 03/07/21.
+//
+
+import UIKit
+
+class TarefaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Tarefas"
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
